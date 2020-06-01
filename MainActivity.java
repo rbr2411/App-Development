1061f0a663bc9fbfecb6de561f3faf0d5a38fd7a
Rohit Rathi <52047227+rbr2411@users.noreply.github.com>
2020-05-31 17:12:36 -1200

    HR Manager Activity button updated

---
+++ MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.healthcareapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void btnEmployee(View view){
+        Intent intent = new Intent(MainActivity.this,EmployeeActivity.class);
+        startActivity(intent);
+    }
+    public void btnManager(View view){
+        Intent intent = new Intent(MainActivity.this,ManagerActivity.class);
+        startActivity(intent);
+    }
+}
